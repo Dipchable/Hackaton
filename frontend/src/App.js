7ef779420c836dafd911286235fdd018e5158364
Michail <pmihail734@gmail.com>
2020-05-09 00:10:41 +0300

    Start

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import React, { PureComponent } from "react";
+
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
