615d6b17863f76c365184848befd635549388635
Michail <pmihail734@gmail.com>
2020-05-09 19:50:08 +0300

    ...

--- Backend/HomePageJob.py
+++ Backend/HomePageJob.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 import json
-import names
+# import names
 
 app = Flask(__name__)
 
 
-@app.route('/HomePageJob', methods=['post', 'get'])
+@app.route('/', methods=['post', 'get'])
 def login():
     test = ''
     with open("test.json", "r") as read_file:
         data = (json.load(read_file))
     if request.method == 'POST':
         test = data
-    return render_template('HomePageJob.jsx', test = test)
+    return render_template('index.html', test = test)
 
 if __name__ == "__main__":
     app.run(debug=False)
