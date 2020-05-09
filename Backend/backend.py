5e6ff2778697240a1891c3eef2759f5efc804c66
Dipchable <korzhikov94@gmail.com>
2020-05-10 00:57:29 +0300

    00:57

--- Backend/backend.py
+++ Backend/backend.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.route('/visit', methods=['post', 'get'])
-def login():
+def visit():
     test = ''
     with open("test.json", "r") as read_file:
         data = (json.load(read_file))
@@ -41,5 +41,24 @@ def register():
             json.dump(data, write_file)
 
     return render_template('register.html')
+@app.route('/loging', methods=['post', 'get'])
+def login():
+    password = ''
+    email = ''
+    message = ''
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        path = email+'.json'
+        if os.path.join(os.path.dirname(__file__),'users',path):
+            with open(os.path.join(os.path.dirname(__file__),'users',path), "r") as read_file:
+                data = json.load(read_file)
+            if data["password"] == password:
+                message = 'password correct'
+            else:
+                message = 'password incorrect'
+
+    return render_template('login.html', message = message)
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000) 
+    app.run(debug = 'true', host='127.0.0.1', port=5000) 
